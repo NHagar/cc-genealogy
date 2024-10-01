dd279ba71f7745804e31a68f65b08e2872234a36
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-10-01 11:39:17 -0500

    madlad queries + placeholders

---
+++ queries/domains/madlad_400_cleaned.sql
@@ -0,0 +1,17 @@
+WITH domains AS (
+SELECT
+    SPLIT_PART(SPLIT_PART(url, '://', 2), '/', 1) AS domain,
+FROM
+-- TODO: I think this is the cleaned split, need to verify
+    READ_JSON('hf://datasets/allenai/MADLAD-400/data-v1p5/*/*.jsonl.gz')
+LIMIT 1000
+)
+SELECT
+    domain,
+    COUNT(*) AS count
+FROM
+    domains
+GROUP BY
+    domain
+ORDER BY
+    count DESC;
---
+++ queries/domains/redpajama_v2.sql
@@ -0,0 +1,1 @@
+-- TODO: This dataset is not stored on HF; we'll have to pull the file URLs and process for each
