f4d1ba85cfb7d3c65dd31114f0ec5c6694dbd27a
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-10-01 11:43:25 -0500

    fineweb query

--- url_query_test.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    url AS original_url,
-    SPLIT_PART(url, '?', 1) AS url_without_params,
-    SPLIT_PART(SPLIT_PART(url, '://', 2), '/', 1) AS domain,
-FROM
-    'hf://datasets/HuggingFaceFW/fineweb-edu/data/*/*.parquet'
-LIMIT 10;
