2b310f2e5b1c6cd0df3650c753fab1f7a745f7ff
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-10-01 11:22:08 -0500

    sampled domain count queries for c4 variants

---
+++ queries/domains/c4_en.sql
@@ -0,0 +1,16 @@
+WITH domains AS (
+SELECT
+    SPLIT_PART(SPLIT_PART(url, '://', 2), '/', 1) AS domain,
+FROM
+    'hf://datasets/allenai/c4/en/*.json.gz'
+LIMIT 1000
+)
+SELECT
+    domain,
+    COUNT(*) AS count
+FROM
+    domains
+GROUP BY
+    domain
+ORDER BY
+    count DESC;
