118f2a5b9de29fb6b969efc3b8fa2145d8a19e45
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-10-03 20:37:13 -0500

    redpajama and dolma

--- queries/domains/dolma.sql
+++ queries/domains/dolma.sql
@@ -1,1 +1,17 @@
--- TODO: This dataset is not stored on HF; we'll have to pull the file URLs and process for each
+WITH domains AS (
+SELECT
+    SPLIT_PART(SPLIT_PART(metadata.url, '://', 2), '/', 1) AS domain,
+FROM
+-- TODO: Will need a coordinator/params to run for every file
+    READ_JSON('https://olmo-data.org/dolma-v1_7/c4-filtered/c4-0000.json.gz')
+LIMIT 1000
+)
+SELECT
+    domain,
+    COUNT(*) AS count
+FROM
+    domains
+GROUP BY
+    domain
+ORDER BY
+    count DESC;
--- queries/domains/redpajama_v2.sql
+++ queries/domains/redpajama_v2.sql
@@ -1,1 +1,17 @@
--- TODO: This dataset is not stored on HF; we'll have to pull the file URLs and process for each
+WITH domains AS (
+SELECT
+    SPLIT_PART(SPLIT_PART(url, '://', 2), '/', 1) AS domain,
+FROM
+-- TODO: Will need a coordinator/params to run for every file
+    READ_JSON('https://data.together.xyz/redpajama-data-v2/v1.0.0/documents/2014-15/0000/en_head.json.gz')
+LIMIT 1000
+)
+SELECT
+    domain,
+    COUNT(*) AS count
+FROM
+    domains
+GROUP BY
+    domain
+ORDER BY
+    count DESC;
