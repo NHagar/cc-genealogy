73f7355605a1b12a4065872bf796119e6c65a608
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-10-15 15:09:23 -0500

    fpaths, handling

--- queries/coordination/file_handling.py
+++ queries/coordination/file_handling.py
@@ -2,28 +2,31 @@ from pathlib import Path
 
 import duckdb
 from huggingface_hub import HfFileSystem
+from tqdm import tqdm
 
 
-def initialize_crawler(pattern: str):
+def initialize_crawler(pattern: str, skip_existing=True):
     fs = HfFileSystem()
     files = fs.glob(pattern)
     sub_name = "_".join(pattern.split("datasets/")[1].split("/")[0:-1])
 
     data_path = Path(f"data/{sub_name}")
-    data_path.mkdir(parents=True, exist_ok=True)
-    to_crawl_path = data_path / "to_crawl.txt"
-    seen_path = data_path / "seen.txt"
-    error_path = data_path / "error.txt"
 
-    with open(to_crawl_path, "w") as f:
-        for file in files:
-            f.write(f"{file}\n")
+    if skip_existing or not data_path.exists():
+        data_path.mkdir(parents=True, exist_ok=True)
+        to_crawl_path = data_path / "to_crawl.txt"
+        seen_path = data_path / "seen.txt"
+        error_path = data_path / "error.txt"
 
-    with open(seen_path, "w") as f:
-        pass
+        with open(to_crawl_path, "w") as f:
+            for file in files:
+                f.write(f"{file}\n")
 
-    with open(error_path, "w") as f:
-        pass
+        with open(seen_path, "w") as f:
+            pass
+
+        with open(error_path, "w") as f:
+            pass
 
     return data_path
 
@@ -48,7 +51,7 @@ def crawl(data_path, query_path, crawl_errors=False):
     else:
         to_crawl = list(set(error) - set(seen))
 
-    for file in to_crawl:
+    for file in tqdm(to_crawl):
         file = file.strip()
         fname = file.split("/")[-1]
         try:
--- queries/run_queries.py
+++ queries/run_queries.py
@@ -1,18 +1,47 @@
+from pathlib import Path
+
 from coordination.file_handling import crawl, initialize_crawler
 
+query_path = Path("queries/domains")
+
 paths = [
-    "hf://datasets/allenai/c4/en/*.json.gz",
-    "hf://datasets/allenai/c4/en.noblocklist/*.json.gz",
-    "hf://datasets/allenai/c4/multilingual/*.json.gz",
-    "hf://datasets/allenai/c4/realnewslike/*.json.gz",
-    "hf://datasets/uonlp/CulturaX/*/*.parquet"
-    "hf://datasets/tiiuae/falcon-refinedweb/data/*.parquet",
-    "hf://datasets/HuggingFaceFW/fineweb/data/*/*.parquet",
-    "hf://datasets/HuggingFaceFW/fineweb-edu/data/*/*.parquet",
-    "hf://datasets/allenai/MADLAD-400/data-v1p5/*/clean*.jsonl.gz",
-    "hf://datasets/allenai/MADLAD-400/data-v1p5/*/noisy*.jsonl.gz",
+    ("hf://datasets/allenai/c4/en/*.json.gz", query_path / "c4_en.sql"),
+    (
+        "hf://datasets/allenai/c4/en.noblocklist/*.json.gz",
+        query_path / "c4_en_noblocklist.sql",
+    ),
+    (
+        "hf://datasets/allenai/c4/multilingual/*.json.gz",
+        query_path / "c4_multilingual.sql",
+    ),
+    (
+        "hf://datasets/allenai/c4/realnewslike/*.json.gz",
+        query_path / "c4_realnewslike.sql",
+    ),
+    ("hf://datasets/uonlp/CulturaX/*/*.parquet", query_path / "culturax.sql"),
+    (
+        "hf://datasets/tiiuae/falcon-refinedweb/data/*.parquet",
+        query_path / "falcon_refinedweb.sql",
+    ),
+    (
+        "hf://datasets/HuggingFaceFW/fineweb/data/*/*.parquet",
+        query_path / "fineweb.sql",
+    ),
+    (
+        "hf://datasets/HuggingFaceFW/fineweb-edu/data/*/*.parquet",
+        query_path / "fineweb_edu.sql",
+    ),
+    (
+        "hf://datasets/allenai/MADLAD-400/data-v1p5/*/clean*.jsonl.gz",
+        query_path / "madlad_400_cleaned.sql",
+    ),
+    (
+        "hf://datasets/allenai/MADLAD-400/data-v1p5/*/noisy*.jsonl.gz",
+        query_path / "madlad_400_raw.sql",
+    ),
 ]
 
-initialize_crawler()
-
-crawl()
+for p in paths:
+    print(f"Initializing crawler for {p[0]}")
+    data_path = initialize_crawler(p[0])
+    crawl(data_path, p[1])
