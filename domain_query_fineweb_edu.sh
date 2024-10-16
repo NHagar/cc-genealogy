1cbf3fb02b34a51b75390d0c8ded356787f8845a
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-10-16 18:17:49 -0500

    bug fixes and parallel runs

---
+++ domain_query_fineweb_edu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=p32491
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --job-name=cc-coordinator
+
+module purge
+
+module load mamba
+
+mamba init
+
+mamba activate /home/nrh146/.conda/envs/cc
+
+python ./queries/run_queries.py --dataset fineweb_edu
--- queries/run_queries.py
+++ queries/run_queries.py
@@ -1,47 +1,56 @@
+import argparse
 from pathlib import Path
 
 from coordination.file_handling import crawl, initialize_crawler
 
 query_path = Path("queries/domains")
 
-paths = [
-    ("hf://datasets/allenai/c4/en/*.json.gz", query_path / "c4_en.sql"),
-    (
+paths = {
+    "c4_en": ("hf://datasets/allenai/c4/en/*.json.gz", query_path / "c4_en.sql"),
+    "c4_en_noblock": (
         "hf://datasets/allenai/c4/en.noblocklist/*.json.gz",
         query_path / "c4_en_noblocklist.sql",
     ),
-    (
+    "c4_multilingual": (
         "hf://datasets/allenai/c4/multilingual/*.json.gz",
         query_path / "c4_multilingual.sql",
     ),
-    (
+    "c4_news": (
         "hf://datasets/allenai/c4/realnewslike/*.json.gz",
         query_path / "c4_realnewslike.sql",
     ),
-    ("hf://datasets/uonlp/CulturaX/*/*.parquet", query_path / "culturax.sql"),
-    (
+    "cultura": (
+        "hf://datasets/uonlp/CulturaX/*/*.parquet",
+        query_path / "culturax.sql",
+    ),
+    "falcon": (
         "hf://datasets/tiiuae/falcon-refinedweb/data/*.parquet",
         query_path / "falcon_refinedweb.sql",
     ),
-    (
+    "fineweb": (
         "hf://datasets/HuggingFaceFW/fineweb/data/*/*.parquet",
         query_path / "fineweb.sql",
     ),
-    (
+    "fineweb_edu": (
         "hf://datasets/HuggingFaceFW/fineweb-edu/data/*/*.parquet",
         query_path / "fineweb_edu.sql",
     ),
-    (
+    "madlad_cleaned": (
         "hf://datasets/allenai/MADLAD-400/data-v1p5/*/clean*.jsonl.gz",
         query_path / "madlad_400_cleaned.sql",
     ),
-    (
+    "madlad_noisy": (
         "hf://datasets/allenai/MADLAD-400/data-v1p5/*/noisy*.jsonl.gz",
         query_path / "madlad_400_raw.sql",
     ),
-]
+}
 
-for p in paths:
-    print(f"Initializing crawler for {p[0]}")
-    data_path = initialize_crawler(p[0], skip_existing=False)
-    crawl(data_path, p[1])
+parser = argparse.ArgumentParser(description="Run queries on selected datasets.")
+parser.add_argument("--dataset", choices=paths.keys(), help="The dataset to process")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    selected_path = paths[args.dataset]
+    print(f"Initializing crawler for {selected_path[0]}")
+    data_path = initialize_crawler(selected_path[0], skip_existing=True)
+    crawl(data_path, selected_path[1])
