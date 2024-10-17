8c3baca2ef13409bedb2e794e3c93de39fdc6eb3
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-10-17 11:50:39 -0500

    filenames

--- domain_query_redpajama.sh
+++ domain_query_redpajama.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=p32491
-#SBATCH --partition=normal
+#SBATCH --partition=long
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
-#SBATCH --time=48:00:00
+#SBATCH --time=168:00:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH --job-name=cc-coordinator
 
@@ -15,4 +15,4 @@ mamba init
 
 mamba activate /home/nrh146/.conda/envs/cc
 
-python ./queries/coordination/dolma.py
+python ./queries/coordination/redpajama.py
