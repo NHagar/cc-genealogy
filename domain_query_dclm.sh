2785a9ba48f5954f49dfae8ff0e0e54ed11d0a28
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-11-04 14:26:02 -0600

    bash scripts

---
+++ domain_query_dclm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=p32491
+#SBATCH --partition=long
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=168:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --job-name=cc-coordinator
+
+module purge
+
+module load mamba
+source ~/.bashrc
+mamba activate /home/nrh146/.conda/envs/cc
+
+python ./queries/run_queries.py --dataset dclm
