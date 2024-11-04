68459078237f9f0f1650116d749ffb1b004c9816
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-11-04 14:16:57 -0600

    zyda 2

---
+++ queries/domains/zyda_2.sql
@@ -0,0 +1,15 @@
+WITH domains AS (
+SELECT
+    SPLIT_PART(SPLIT_PART(url, '://', 2), '/', 1) AS domain,
+FROM
+    '{fpath}'
+)
+SELECT
+    domain,
+    COUNT(*) AS count
+FROM
+    domains
+GROUP BY
+    domain
+ORDER BY
+    count DESC
