6afbd6b2394daa65cb7abba59252f090bad06650
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-11-04 15:13:42 -0600

    exclude databases currently running collection

--- consolidate_databases.sh
+++ consolidate_databases.sh
@@ -14,11 +14,11 @@ source ~/.bashrc
 mamba activate /home/nrh146/.conda/envs/cc
 
 python queries/consolidate_domain_data.py --database data/allenai_c4_en/database.db
-python queries/consolidate_domain_data.py --database data/allenai_MADLAD-400_data-v1p5_*/database.db
+# python queries/consolidate_domain_data.py --database data/allenai_MADLAD-400_data-v1p5_*/database.db
 python queries/consolidate_domain_data.py --database data/dolma/database.db
 python queries/consolidate_domain_data.py --database data/HuggingFaceFW_fineweb-edu_data_*/database.db
-python queries/consolidate_domain_data.py --database data/HuggingFaceFW_fineweb_data_*/database.db
-python queries/consolidate_domain_data.py --database data/mlfoundations_dclm-baseline-1.0-parquet_filtered_**/database.db
+# python queries/consolidate_domain_data.py --database data/HuggingFaceFW_fineweb_data_*/database.db
+# python queries/consolidate_domain_data.py --database data/mlfoundations_dclm-baseline-1.0-parquet_filtered_**/database.db
 python queries/consolidate_domain_data.py --database data/tiiuae_falcon-refinedweb_data/database.db
 python queries/consolidate_domain_data.py --database data/uonlp_CulturaX_*/database.db
-python queries/consolidate_domain_data.py --database data/Zyphra_Zyda-2_data_**/database.db
+# python queries/consolidate_domain_data.py --database data/Zyphra_Zyda-2_data_**/database.db
