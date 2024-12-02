68c4f90dd0d6db77c1167ff108297b33c44c7b4b
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-12-02 15:15:14 -0600

    fix bug that destroyed data on rerun

--- queries/condense_and_upload.py
+++ queries/condense_and_upload.py
@@ -1,7 +1,8 @@
+import os
 from argparse import ArgumentParser
 
 from dotenv import load_dotenv
-from file_operations import combine_parquet_files
+from file_operations import combine_parquet_files, upload_directory_to_hf
 
 load_dotenv()
 
@@ -10,11 +11,11 @@ parser.add_argument("--dataset", type=str, required=True)
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    directory = f".data/urls/output/dataset={args.dataset}"
+    directory = f"./data/urls/output/dataset={args.dataset}"
     combine_parquet_files(directory)
-    # upload_directory_to_hf(
-    #     directory,
-    #     f"nhagar/{args.dataset}_urls",
-    #     token=os.getenv("HF_TOKEN"),
-    #     private=True,
-    # )
+    upload_directory_to_hf(
+        directory,
+        f"nhagar/{args.dataset}_urls",
+        token=os.getenv("HF_TOKEN"),
+        private=True,
+    )
