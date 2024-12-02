6dc41651c5e55dad6e06df3c14e07c8d80742f65
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-12-02 17:18:32 -0600

    bump up queue time

--- data_export.sh
+++ data_export.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=p32491
-#SBATCH --partition=short
+#SBATCH --partition=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=1:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=32G
 #SBATCH --job-name=export
 
