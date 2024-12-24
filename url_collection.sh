8990be8cc459b94c96f188cfef364c7f1f810fba
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-12-24 11:27:19 -0500

    url collection queue

--- url_collection.sh
+++ url_collection.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=p32491
-#SBATCH --partition=short
+#SBATCH --partition=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
-#SBATCH --time=1:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH --job-name=url-collection
 
@@ -13,4 +13,4 @@ module load mamba
 source ~/.bashrc
 mamba activate /home/nrh146/.conda/envs/cc
 
-python -u ./queries/url_collection.py --target_dataset cultura
+python -u ./queries/url_collection.py
