f0aff9810af35e58208e5776a38806857bbf7593
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-01-05 08:12:31 -0800

    double ram for export job

--- data_export.sh
+++ data_export.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=32G
+#SBATCH --mem-per-cpu=64G
 #SBATCH --job-name=export
 
 module purge
@@ -13,4 +13,4 @@ module load mamba
 source ~/.bashrc
 mamba activate /home/nrh146/.conda/envs/cc
 
-python -u ./queries/upload_to_hf.py --dataset cultura --scratch
+python -u ./queries/upload_to_hf.py --dataset fineweb --scratch
