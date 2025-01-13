e5baa965df187c0c842ef0277272671284c3bde3
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-01-13 11:31:31 -0600

    pipeline script

--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,19 @@
 from argparse import ArgumentParser
 
+import duckdb
+
 from queries.crawler import initialize_and_run_crawler
 
 parser = ArgumentParser()
 parser.add_argument("--crawl-errors", action="store_true")
 parser.add_argument("--target-dataset", type=str)
 
+con = duckdb.connect()
+
 # url collection
 initialize_and_run_crawler(**vars(parser.parse_args()))
+
+# analysis steps
+
+
+# data upload
