39dbf1dedb5d30820e6aeb214ff80c955604727f
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-01-13 11:28:36 -0600

    move url collection into crawler module

--- queries/crawler.py
+++ queries/crawler.py
@@ -8,7 +8,25 @@ from typing import Dict, List
 import duckdb
 import pandas as pd
 from dotenv import load_dotenv
-from huggingface_hub import HfFileSystem
+from huggingface_hub import HfFileSystem, login
+
+patterns_hf = {
+    "c4_en": "hf://datasets/allenai/c4/en/*.json.gz",
+    "cultura": "hf://datasets/uonlp/CulturaX/*/*.parquet",
+    "falcon": "hf://datasets/tiiuae/falcon-refinedweb/data/*.parquet",
+    "fineweb": "hf://datasets/HuggingFaceFW/fineweb/data/*/*.parquet",
+    "fineweb_edu": "hf://datasets/HuggingFaceFW/fineweb-edu/data/*/*.parquet",
+    "madlad_cleaned": "hf://datasets/allenai/MADLAD-400/data-v1p5/*/clean*.jsonl.gz",
+    "zyda_2": "hf://datasets/Zyphra/Zyda-2/data/**/*.parquet",
+    "dclm": "hf://datasets/mlfoundations/dclm-baseline-1.0-parquet/filtered/**/*.parquet",
+    # "madlad_noisy": "hf://datasets/allenai/MADLAD-400/data-v1p5/*/noisy*.jsonl.gz",
+    # "c4_en_noblock": "hf://datasets/allenai/c4/en.noblocklist/*.json.gz",
+    # "c4_multilingual": "hf://datasets/allenai/c4/multilingual/*.json.gz",
+    # "c4_news": "hf://datasets/allenai/c4/realnewslike/*.json.gz",
+}
+
+with open("./data/dolma_urls.txt", "r") as f:
+    data_other = {"dolma": [i.strip() for i in f.readlines()]}
 
 
 class Crawler:
@@ -158,3 +176,20 @@ class Crawler:
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
             executor.map(self.process_file, [row for _, row in to_crawl_df.iterrows()])
+
+
+def initialize_and_run_crawler(crawl_errors: bool, target_dataset: str):
+    login(token=os.getenv("HF_TOKEN"))
+    url_crawler = Crawler("urls", use_scratch=True)
+
+    # Initialize the crawler
+    print("Initializing crawler...")
+    needs_populated = url_crawler.initialize_crawler()
+    if needs_populated:
+        print("Populating crawler...")
+        url_crawler.populate_hf(patterns_hf)
+        url_crawler.populate_other(data_other)
+
+    # Run the crawler with command line arguments
+    print("Running crawler...")
+    url_crawler.crawl(crawl_errors=crawl_errors, target_dataset=target_dataset)
