eb144b4817b7d34cca34730fc71c8b8d18d5c013
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-01-27 18:02:02 -0600

    move stuff around

--- queries/analysis/domain_counts.sql
+++
@@ -1,9 +0,0 @@
-COPY (
-    SELECT 
-        split_part(url, '/', 3) as domain, 
-        COUNT(*) as freq 
-    FROM read_parquet('DATASET_PATTERN') 
-    GROUP BY domain 
-    ORDER BY freq DESC
-) 
-TO 'OUTPUT_PATH';
