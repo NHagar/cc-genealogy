4ede4a5ed7aab1199fb9a5f6cac1cf05ac36b2b2
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-02-12 13:56:57 -0600

    slurm script

---
+++ hf_collection_slurm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=p32491  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=normal  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=32 ## how many cpus or processors do you need on each computer
+#SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
+#SBATCH --mem-per-cpu=6G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=url_collection  ## When you run squeue -u NETID this is how you can identify the job
+
+module purge all
+
+uv run derived_dataset_pipeline.py --dataset c4_realnewslike
