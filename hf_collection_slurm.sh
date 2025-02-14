5d4399a7f82fa284369048e7ca1b4fd1fece190d
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-02-14 15:10:21 -0600

    try boosting core count again

--- hf_collection_slurm.sh
+++ hf_collection_slurm.sh
@@ -2,9 +2,9 @@
 #SBATCH --account=p32491  ## YOUR ACCOUNT pXXXX or bXXXX
 #SBATCH --partition=normal  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=1 ## how many computers do you need
-#SBATCH --ntasks-per-node=8 ## how many cpus or processors do you need on each computer
+#SBATCH --ntasks-per-node=16 ## how many cpus or processors do you need on each computer
 #SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
-#SBATCH --mem-per-cpu=16G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --mem-per-cpu=8G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=url_collection  ## When you run squeue -u NETID this is how you can identify the job
 
 module purge all
