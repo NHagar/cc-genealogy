64f62cf49a8a72dee5f079a3101e953cf1d763cb
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-02-16 17:07:45 -0600

    add retry for file access errors

--- src/io/file_operations.py
+++ src/io/file_operations.py
@@ -1,5 +1,9 @@
+import json
+
+import dask.bag as db
 import pandas as pd
 from huggingface_hub import HfApi
+from tenacity import retry, wait_exponential
 
 
 def create_hf_repo(data: pd.DataFrame, dataset_name: str, token: str) -> None:
@@ -22,3 +26,12 @@ def create_hf_repo(data: pd.DataFrame, dataset_name: str, token: str) -> None:
     data.to_parquet(f"hf://datasets/{dataset_name}/data", index=False)
 
     print(f"Successfully uploaded dataset as {dataset_name}")
+
+
+@retry(wait=wait_exponential(multiplier=1, min=4, max=10))
+def read_text_with_retry(batch):
+    bag = db.read_text(batch, compression="gzip").map(
+        lambda x: {"url": json.loads(x)["url"]}
+    )
+
+    return bag
