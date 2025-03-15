eceef8bbea0963e76f386e636e12bb05beb1554f
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-03-15 16:04:10 -0400

    more time

--- hf_collection_slurm.sh
+++ hf_collection_slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=normal  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
-#SBATCH --time=12:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
+#SBATCH --time=28:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
 #SBATCH --mem-per-cpu=16G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=url_collection  ## When you run squeue -u NETID this is how you can identify the job
 
