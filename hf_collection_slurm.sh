d1bec2b17213d50231270978b5b770c178186761
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-03-14 19:13:07 -0500

    updated slurm config for test

--- hf_collection_slurm.sh
+++ hf_collection_slurm.sh
@@ -2,11 +2,11 @@
 #SBATCH --account=p32491  ## YOUR ACCOUNT pXXXX or bXXXX
 #SBATCH --partition=normal  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=1 ## how many computers do you need
-#SBATCH --ntasks-per-node=16 ## how many cpus or processors do you need on each computer
-#SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
-#SBATCH --mem-per-cpu=8G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
+#SBATCH --time=12:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
+#SBATCH --mem-per-cpu=16G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=url_collection  ## When you run squeue -u NETID this is how you can identify the job
 
 module purge all
 
-uv run derived_dataset_pipeline.py --source-repo allenai/c4 --configs en --batch-size 200000 --num-proc 16
+uv run new_pipeline.py
