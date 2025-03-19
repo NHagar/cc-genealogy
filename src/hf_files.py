414c12c2a0b4cb8310c8b9a77d3e6f4e48e69b06
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-03-19 14:06:52 -0500

    better ruleset structuring

--- src/hf_files.py
+++ src/hf_files.py
@@ -4,30 +4,34 @@ import duckdb
 from huggingface_hub import HfApi
 
 dataset_rules = {
-    "multilingual_c4": {
-        "prefix": "multilingual/",
-        "suffix": ".json.gz",
-        "exclude": "-validation",
+    "allenai/c4": {
+        "variants": {
+            "multilingual": {
+                "prefix": "multilingual/",
+                "suffix": ".json.gz",
+                "exclude": "-validation",
+            }
+        }
     }
 }
 
 
-def get_files_for_dataset(dataset: str, ruleset: str) -> List[str]:
+def get_files_for_dataset(dataset: str, variant: str) -> List[str]:
     """
-    Get the formatted list of filepaths that match the ruleset for a given dataset.
+    Get the formatted list of filepaths that match the variant for a given dataset.
 
     Args:
         dataset (str): The dataset name to get files for.
-        ruleset (str): The ruleset to use for filtering the files.
+        variant (str): The variant to use for filtering the files.
 
     Returns:
-        List[str]: The list of filepaths that match the ruleset for the dataset.
+        List[str]: The list of filepaths that match the variant for the dataset.
     """
     api = HfApi()
     repo_files = api.list_repo_files(dataset, repo_type="dataset")
-    prefix = dataset_rules[ruleset]["prefix"]
-    suffix = dataset_rules[ruleset]["suffix"]
-    exclude = dataset_rules[ruleset]["exclude"]
+    prefix = dataset_rules[variant]["prefix"]
+    suffix = dataset_rules[variant]["suffix"]
+    exclude = dataset_rules[variant]["exclude"]
     files = [
         f
         for f in repo_files
@@ -38,17 +42,17 @@ def get_files_for_dataset(dataset: str, ruleset: str) -> List[str]:
     return files
 
 
-def get_file_table(dataset: str, ruleset: str, con: duckdb.DuckDBPyConnection):
+def get_file_table(dataset: str, variant: str, con: duckdb.DuckDBPyConnection):
     """
-    Get the formatted table of filepaths that match the ruleset for a given dataset.
+    Get the formatted table of filepaths that match the variant for a given dataset.
 
-    This function creates or retrieves a DuckDB table for the specified dataset and ruleset.
+    This function creates or retrieves a DuckDB table for the specified dataset and variant.
     If the table already exists, it returns the uncollected files as a DataFrame.
     If not, it creates a new table with the filepaths and their collection status.
 
     Args:
         dataset (str): The dataset name to get files for.
-        ruleset (str): The ruleset to use for filtering the files.
+        variant (str): The variant to use for filtering the files.
         con (duckdb.DuckDBPyConnection): The connection to the DuckDB database.
 
     Returns:
@@ -56,10 +60,10 @@ def get_file_table(dataset: str, ruleset: str, con: duckdb.DuckDBPyConnection):
                          for files that have not been collected yet.
     """
     # check if table exists and return if it does
-    table_name = f"{dataset.replace('/', '_')}_{ruleset}"
+    table_name = f"{dataset.replace('/', '_')}_{variant}"
     tables = con.execute("SHOW TABLES").fetchdf()
     if table_name not in tables["name"].values:
-        files = get_files_for_dataset(dataset, ruleset)
+        files = get_files_for_dataset(dataset, variant)
         con.execute(f"CREATE TABLE {table_name} (filepath VARCHAR, collected BOOLEAN)")
         con.execute(
             f"INSERT INTO {table_name} VALUES {','.join([f"('{f}', false)" for f in files])}"
