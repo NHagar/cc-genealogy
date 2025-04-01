0316fcb35014833ec4c8b4a4e02567bf1a6598aa
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-04-01 18:33:05 -0500

    email alerts

--- hf_collection_slurm.sh
+++ hf_collection_slurm.sh
@@ -6,6 +6,8 @@
 #SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
 #SBATCH --mem-per-cpu=8G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=url_collection  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --mail-user=nicholas.hagar@northwestern.edu
+#SBATCH --mail-type=ALL
 
 module purge all
 
