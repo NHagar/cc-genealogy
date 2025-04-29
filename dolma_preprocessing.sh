cae1df8718e471e7eab513cbd07d805459f64c4a
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-04-29 13:28:26 -0500

    fixes

--- dolma_preprocessing.sh
+++ dolma_preprocessing.sh
@@ -4,8 +4,6 @@ git clone https://huggingface.co/datasets/allenai/dolma cache-dolma
 
 rm -f cache-dolma/urls/*-sample*.txt
 
-mkdir -p data
-
 # Process each txt file
 for file in cache-dolma/urls/*.txt; do
     # Extract filename without extension
@@ -13,13 +11,13 @@ for file in cache-dolma/urls/*.txt; do
     echo "Processing $base_name"
     
     # Create output directory
-    mkdir -p "data/dolma_${base_name}"
+    mkdir -p "/projects/p32491/cc-genealogy/data/dolma_${base_name}"
     
     # Filter lines matching patterns and chunk into batches of 400
     grep -E '/c4|/cc-|/falcon-refinedweb' "$file" | \
     awk '{
         batch = int((NR-1) / 400) + 1;
-        output_file = "data/dolma_" line_base_name "/download_urls_batch_" batch ".txt";
+        output_file = "/projects/p32491/cc-genealogy/data/dolma_" line_base_name "/download_urls_batch_" batch ".txt";
         print $0 > output_file;
     }' line_base_name="$base_name"
     
