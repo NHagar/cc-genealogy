f89bf1d4c3566b02cca1445f34f9f43f0b38cde1
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-05-06 19:01:14 -0500

    more cores

--- kl_divergence.sh
+++ kl_divergence.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=p32491  ## YOUR ACCOUNT pXXXX or bXXXX
 #SBATCH --partition=normal  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=1 ## how many computers do you need
-#SBATCH --ntasks-per-node=4 ## how many cpus or processors do you need on each computer
+#SBATCH --ntasks-per-node=8 ## how many cpus or processors do you need on each computer
 #SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
 #SBATCH --mem-per-cpu=8G ## how much RAM do you need per node (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=kl_divergence  ## When you run squeue -u NETID this is how you can identify the job
