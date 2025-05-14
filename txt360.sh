7ecbe72ff14a0aefb17ca527ecf555bfdb7582c1
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-05-13 22:24:03 -0500

    sbatch

---
+++ txt360.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=p32491  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=normal  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=1                # Ensure tasks run on single nodes unless needed otherwise
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this parameter)
+#SBATCH --job-name=txt360  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --mail-user=nicholas.hagar@northwestern.edu
+#SBATCH --mail-type=ALL
+
+module purge all
+
+uv run txt360_pipeline.py
